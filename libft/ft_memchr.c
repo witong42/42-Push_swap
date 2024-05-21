ac2ce37de0b4fc1717b57011b6fc29beb301a48f
williamtng <williamtong1207@gmail.com>
2024-05-21 21:23:57 +0200

    Update

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -1,12 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/20 21:42:50 by witong            #+#    #+#             */
-/*   Updated: 2024/05/20 22:03:34 by witong           ###   ########.fr       */
+/*   Created: 2024/05/20 21:42:39 by witong            #+#    #+#             */
+/*   Updated: 2024/05/21 21:22:23 by witong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	int	i;
+	unsigned char *ucs;
+
+	ucs = (unsigned char *)s;
+	i = 0;
+	while (i < n)
+	{
+		if (ucs[i] == (unsigned char)c)
+			return ((void *)&ucs[i]);
+		i++;
+	}
+	return (NULL);
+}
--- libft/ft_strchr.c
+++ libft/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 21:42:46 by witong            #+#    #+#             */
-/*   Updated: 2024/05/20 21:59:08 by witong           ###   ########.fr       */
+/*   Updated: 2024/05/21 21:12:44 by witong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,15 @@
 char	*strchr(const char *s, int c)
 {
 	int	i;
-	char	cc;
 
-	cc = (char)c;
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == cc)
+		if (s[i] == (char)c)
 			return((char *)&s[i]);
 		i++;
 	}
-	if (cc == '\0')
+	if ((char)c == '\0')
 		return ((char *)&s[i]);
 	return (0);
 }
