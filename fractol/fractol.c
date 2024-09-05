ff2951910794fef404068d3da47db5739c33d7df
williamtng <30389189+witong42@users.noreply.github.com>
2024-09-05 14:28:12 +0000

    Update

---
+++ fractol/fractol.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fractol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 08:01:34 by witong            #+#    #+#             */
+/*   Updated: 2024/08/28 08:09:34 by witong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+We need fractal Julia and Mandelbroth and krystal or fibo
+cmd line to discipline fractal
+shape with x y coordinates
+ESC and X close no leaks
+./fractol mandelbrot
+./fractol julia <real> <imgry>
+TL,DR of the app
+*/
+
+int main(int ac, char **av)
+{
+	if (ac == 2 ) //TODO mandelbrot && julia
+	{
+		app
+	}
+	else
+		//TODO ERROR
+}
