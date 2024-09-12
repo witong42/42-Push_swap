3fc1b0614c5842b35ba28a52c6751db49fc5ac7e
williamtng <williamtong1207@gmail.com>
2024-09-12 07:14:18 +0200

    Update

--- fractol/srcs/init.c
+++ fractol/srcs/init.c
@@ -1,12 +1,35 @@
 #include "fractol.h"
 
+void close_window(t_fractal *fract)
+{
+	if (fract->img)
+		mlx_destroy_image(fract->mlx, fract->img);
+	if (fract->win)
+		mlx_destroy_window(fract->mlx, fract->win);
+	if (fract->mlx)
+	{
+		mlx_destroy_display(fract->mlx);
+		free(fract->mlx);
+	}
+	free(fract);
+	exit(EXIT_FAILURE);
+}
+
 void	fract_init(t_fractal *fract)
 {
 	fract->mlx = mlx_init();
-	fract->win = mlx_new_window(fract->mlx, SIZE, SIZE, "Fractol");
+	if (!fract->mlx)
+		close_window(fract);
+	fract->win = mlx_new_window(fract->mlx, SIZE, SIZE, fract->name);
+	if (!fract->win)
+		close_window(fract);
 	fract->img = mlx_new_image(fract->mlx, SIZE, SIZE);
-	fract->ptr_img = mlx_get_data_addr(fract->img,
+	if (!fract->img)
+		close_window(fract);
+	fract->pxl_img = mlx_get_data_addr(fract->img,
 					&fract->bpp,
 					&fract->size_line,
 					&fract->endian);
+	if (!fract->pxl_img)
+		close_window(fract);
 }
--- fractol/srcs/main.c
+++ fractol/srcs/main.c
@@ -6,29 +6,26 @@
 /*   By: witong <witong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 08:01:34 by witong            #+#    #+#             */
-/*   Updated: 2024/09/11 16:01:45 by witong           ###   ########.fr       */
+/*   Updated: 2024/09/11 17:26:49 by witong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-/* Function to close window */
-int close_window(int keycode, void *param)
-{
-	(void)param;
-	if (keycode == ESC_KEY)
-		exit(0);
-	return (0);
-}
 /* Main that manage the input and window */
 int main(int ac, char **av)
 {
-	t_fractal	fract;
+	t_fractal	*fract;
 
 	if (ac >= 2 && ac <= 4 && (ft_strcmp(av[1], "mandelbrot") == 0
 		|| ft_strcmp(av[1], "julia") == 0))
 	{
-		fract_init(*fract);
+		fract = malloc(sizeof(t_fractal));
+		if (!fract)
+			exit(EXIT_FAILURE);
+		fract->name = av[1];
+		fract_init(fract);
+		fract_render(fract);
 		mlx_key_hook(fract->win, close_window, NULL);
 		mlx_loop(fract->mlx);
 	}
